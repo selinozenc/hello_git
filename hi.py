2e37beef4edaab81e29ce1a1065ff54c08f9ad91
selinozenc <selin_ozenc90@hotmail.com>
2021-05-14 23:52:02 +0300

    new_feature branch'ine HalHatirSor isimli yeni fonksiyon eklendi.

--- hi.py
+++ hi.py
@@ -3,5 +3,13 @@ print("HelloSelo:)")
 
 print("Yeni bir şeyler yapmak gerenkk... =) ")
 
-print("Alakasız bilgilerde BUGÜN... :P")
+
+
+print("ssssssssssssss")
+
+
+def HalHatirSor():
+    print("Nasılız? Keyifler yerindedir inşallah :) ")
+
+
 
