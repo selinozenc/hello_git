5f84d8c97173e53ec508b78c9e888d1a62d7b6f3
selinozenc <selin_ozenc90@hotmail.com>
2021-05-29 02:08:04 +0300

    hal hatır sorusuna karşın olası cevaplar eklendi:P

--- hi.py
+++ hi.py
@@ -13,5 +13,12 @@ print("ssssssssssssss")
 def HalHatirSor():
     print("Nasılız? Keyifler yerindedir inşallah :) ")
 
+def GuncelHal(guncelHal):
+    if (guncelHal == "eh işte"):
+        print("İç güveysinden halliceyiz:P")
+    elif (guncelHal == "süper"):
+        print("Mikkemmelll hissediyorum. Anlatmaya gerek yok, görüyorsunuz!:P")
+    else:
+        print("İyi diyelim iyi olalım:D")
 
 
